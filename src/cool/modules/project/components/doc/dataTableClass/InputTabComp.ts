768321bd955055df702e1d834f76d6214cccff8f
maiersk <poilyoungman@foxmail.com>
2022-01-29 02:32:00 +0800

    update: delete old code

--- src/cool/modules/project/components/doc/dataTableClass/InputTabComp.ts
+++
@@ -1,9 +0,0 @@
-import BaseTabComp, { TabCompOption } from './BaseTabComp'
-import { ElInput } from 'element-plus'
-
-export default class InputTabComp extends BaseTabComp {
-  constructor(name: string, options: TabCompOption) {
-    super(name, options)
-    this.tag = ElInput
-  }
-}
