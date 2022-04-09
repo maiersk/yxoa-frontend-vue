ae76606bf5d701271a1a0d5c793e783d15513919
maiersk <poilyoungman@foxmail.com>
2022-04-09 18:24:00 +0800

    update: fix project user

--- src/cool/modules/project/components/project/tabs-user.vue
+++ src/cool/modules/project/components/project/tabs-user.vue
@@ -14,11 +14,7 @@
 
 		<el-row>
 			<!-- 数据表格 -->
-			<cl-table	:ref="setRefs('table')"
-				v-bind="table"
-				style="max-height:700px"
-				:filter-method="handelFilter"
-			>
+			<cl-table :ref="setRefs('table')" v-bind="table" style="max-height: 700px">
 				<!-- 头像 -->
 				<template #column-headImg="{ scope }">
 					<cl-avatar
@@ -31,11 +27,11 @@
 			</cl-table>
 		</el-row>
 
-		<!-- <el-row type="flex">
-			<cl-flex1 /> -->
-			<!-- 分页控件 -->
-			<!-- <cl-pagination />
-		</el-row> -->
+		<el-row type="flex">
+			<cl-flex1 />
+			分页控件
+			<cl-pagination />
+		</el-row>
 
 		<!-- 新增、编辑 -->
 		<cl-upsert :ref="setRefs('upsert')" v-bind="upsert" />
@@ -43,22 +39,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, reactive } from "vue";
+import { defineComponent, onMounted, reactive, inject } from "vue";
 import { CrudLoad, Table, Upsert } from "@cool-vue/crud/types";
 import { useCool } from "/@/cool";
 
 export default defineComponent({
 	name: "project-user",
-	props: {
-		project: {
-			type: Object,
-			default() {
-				return {};
-			}
-		}
-	},
+	props: {},
 	setup(props: any) {
 		const { refs, setRefs, service } = useCool();
+		const project = inject<any>("project");
 
 		// 新增、编辑配置
 		const upsert = reactive<Upsert>({
@@ -93,53 +83,36 @@ export default defineComponent({
 				{ label: "姓名", prop: "name" },
 				{ label: "昵称", prop: "nickName" },
 				{ label: "手机", prop: "phone" },
-				{ label: "部门", prop: "department" },
-				{ label: "工作内容", prop: "workCtx" },
+				{ label: "工作内容", prop: "workCtx" }
 			]
 		});
 
 		onMounted(async () => {
-			// console.log(props.project.id)
 
-		})
-
-		const handelFilter = (value: any, row: any, column: any) => {
-			const property = column['property'];
-			return row[property] === value;
-		}
+		});
 
 		// crud 加载
 		async function onLoad({ ctx, app }: CrudLoad) {
-			// const res = await service.project.project.getUsers(props.project.id)
-			// console.log(res);
-			ctx.service(service.base.sys.user).done();
+			ctx.service({
+				async page() {
+					return service.project.prjuser.page({
+						projectId: project.value.id
+					});
+				},
+				list() {
+					return service.project.prjuser.list();
+				},
+				add(data) {
+					return service.project.prjuser.add({
+						projectId: project.value.id,
+						...data
+					});
+				},
+				delete(data) {
+					return service.project.prjuser.delete(data);
+				}
+			}).done();
 			app.refresh();
-			// setTimeout(() => {
-			// 	// 绑定 service
-			// 	ctx.service({
-			// 		async page() {
-			// 			return Promise.resolve({
-			// 				list: []
-			// 			})
-			// 		},
-			// 		// list() {
-			// 		// 	return service.project.project.getUsers(props.project.id)
-			// 		// },
-			// 		add(data) {
-			// 			 return service.project.project.addUser({
-			// 				projectId: props.project.id,
-			// 				userId: data.userId,
-			// 				workCtx: data.workCtx
-			// 			})
-			// 		},
-			// 		delete(data) {
-			// 			return service.project.project.delUser({
-			// 				userId: data.userId
-			// 			})
-			// 		}
-			// 	}).done();
-			// 	app.refresh();
-			// }, 1000)
 		}
 
 		return {
@@ -148,7 +121,7 @@ export default defineComponent({
 			table,
 			onLoad,
 			setRefs,
-			handelFilter,
+			project
 		};
 	}
 });
---
+++ src/cool/modules/project/service/prjuser.ts
@@ -0,0 +1,7 @@
+import { BaseService, Service } from "/@/cool";
+
+@Service("project/prjuser")
+class ProjectAppUserService extends BaseService {
+}
+
+export default ProjectAppUserService;
